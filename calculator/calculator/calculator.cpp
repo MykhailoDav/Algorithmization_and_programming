a4ded0648f15d79420be9b09e857eb36a772c826
Mykhailo <davudenko.mukhailo@gmail.com>
2024-02-26 08:52:36 +0200

    first element

---
+++ calculator/calculator/calculator.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream> // підключаємо бібліотеку вводу-виводу
+// - містить опис всіх функцій, які забезпечують ввід-вивід
+#include <Windows.h> // підключаємо бібліотеку, яка забезпечує відображення кирилиці
+using namespace std; // використовуємо стандартний простір імен
+// - після цього доступ до ресурсів
+// бібліотеки вводу-виводу буде не громіздкий, а компактний
+int main() 
+{	SetConsoleCP(1251); 
+	SetConsoleOutputCP(1251); 
+	double a;
+	double b;
+	double rez;
+	char dia;
+	cout << "a="; cin >> a;
+
+	cout << "введіть дію (+ - * /) : "; cin >> dia;
+	
+	cout << "b="; cin >> b;
+	
+	switch (dia)
+	{
+	case '+' :
+		rez = a + b;
+		break;
+	case '-':
+		rez = a - b;
+		break;
+	case '*':
+		rez = a * b;
+		break;
+	case '/':
+		if (b != 0)
+			rez = a / b;
+		else
+			cout << "на 0 ділити не можна" << endl;
+		break;
+	default:	
+		cout << "Ви ввели не дійсну дію" << endl;
+		break;
+	}
+	cout << "результат: " << rez << endl;
+	return 0;
+} 
